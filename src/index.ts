5f78bc0fad9fb2efaffd187f9c11e742c7e0c2f8
Karan Sanjeev <yo@alphaman.me>
2021-06-11 14:04:16 +0530

    Scrape data off Stucor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("lol");
+import cheerio from "cheerio";
+import axios from "axios";
+
+const scrapeData = async () => {
+  let blogEntries: BlogEntry[] = [];
+  let blogTimings = [];
+
+  const { data } = await axios.get("https://stucor.in/category/annauniv/");
+  const $ = cheerio.load(data);
+
+  $(".p-url").each((_, element) => {
+    blogEntries.push({
+      title: element.attribs["title"],
+      link: element.attribs["href"],
+    });
+  });
+
+  $("abbr.date").each((_, element) => {
+    blogTimings.push(element.attribs["title"]);
+  });
+
+  return blogEntries.map((blog, index) => ({
+    ...blog,
+    time: blogTimings[index],
+  }));
+};
+
+scrapeData().then(console.log).catch(console.error);
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+interface BlogEntry {
+  title: string;
+  link: string;
+}
